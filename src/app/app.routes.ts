cf27dd064a295a40579a26d1fc234e9d5d46a9c2
Kaique Oliveira <kaique.oliveira@amazoniasoftware.com.br>
2025-11-21 21:16:26 -0300

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
+
+export const routes: Routes = [
+{path: '' , component: FormularioComponent}
+];
